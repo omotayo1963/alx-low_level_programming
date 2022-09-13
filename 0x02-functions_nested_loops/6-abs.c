0ba3f8efbfeeb715ab2083e2a04dc29d87b9fdc6
omotayo1963 <adeniyimuizudeen40@gmail.com>
2022-09-13 12:35:39 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _abs - this program prints laphabet follow by a new line
+ *
+ * @num: character value
+ *
+ * Return: num
+ */
+
+int _abs(int num)
+{
+	if (num < 0)
+	{
+		num = num * -1;
+		return (num);
+	}
+	else
+	{
+		 num = num * 1;
+		 return (num);
+	}
+}
