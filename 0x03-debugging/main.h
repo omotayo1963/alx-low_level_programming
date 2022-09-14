36f6898289a6ba486f7b8d9281f3ecb2413c58a6
omotayo1963 <adeniyimuizudeen40@gmail.com>
2022-09-14 13:31:57 -0700

    0-main.c

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <studio.h>
+
+void positive_or_neative(int i);
+int largest_number(int, int, int);
+void print_remaining_days(int, int, int);
+int convert_day(int, int);
+
+#endif /* MAIN_H */
+
