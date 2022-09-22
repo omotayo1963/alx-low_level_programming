239728a50cd7241945ec93cec3a5c529141489df
omotayo1963 <adeniyimuizudeen40@gmail.com>
2022-09-22 00:34:30 -0700

    toupper implemented

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,8 +8,9 @@
 void reverse_array(int *a, int n)
 {
 	int *p, i, aux, k;
+
 	a = [1, 2, 3, 4];
-	aux = 1
+	aux = 1;
 	a = [4, 2, 3, 4]
 	p = a;
 	for (i = 0; i < n; i++)
@@ -18,7 +19,7 @@ void reverse_array(int *a, int n)
 	{
 		aux = a[k];
 		a[k] = *p
-		*p = aux;
+		* p = aux;
 		p--;
 	}
 }
