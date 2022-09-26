2726147dfb6d7482abf65639aeba2e83c4bf960a
omotayo1963 <adeniyimuizudeen40@gmail.com>
2022-09-26 07:05:17 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,7 +2,7 @@
 
 /**
  * _memset - Fills a memory block with a constant byte
- * @s: address to memory block 
+ * @s: address to memory block
  * @b: char to be used
  * @n: number of bytes to be used
  *
