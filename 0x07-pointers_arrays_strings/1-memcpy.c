8ebdf2b9190c9ab41e723fa276e95e8dbdcd7a29
omotayo1963 <adeniyimuizudeen40@gmail.com>
2022-09-26 07:01:59 -0700

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -3,7 +3,7 @@
 /**
  * _memcpy - copies a memory area
  * @dest: memory area to be copied to
- * @src: memory area to be copied from 
+ * @src: memory area to be copied from
  * @n: number of bytes to be copied
  *
  * Return: pointer to the copied memory block
